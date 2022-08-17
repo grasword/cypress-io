846309d9deaadefaa46b1402b1301415803366c6
Oleg Zhmaev <grasword6@gmail.com>
2022-08-17 21:10:20 +0300

    implemented `header` unit tests for links

---
+++ cypress/e2e/unit/home-page/header.cy.js
@@ -0,0 +1,28 @@
+describe('Header', () => {
+  beforeEach(() => {
+    cy.visit('https://www.cypress.io/')
+  })
+
+  it('Navigation menu links has a valid href', () => {
+    cy.get('header * ul > li > a').as('navItems')
+
+    cy.get('@navItems').should('have.length', 11)
+
+    cy.get('@navItems').each((item) => {
+      cy.wrap(item, { log: false })
+        .invoke({ log: false }, 'attr', 'href')
+        .then((href) => {
+          cy.log(`${href} should have valid href`)
+          cy.request(href).its('status').should('eq', 200)
+        })
+    })
+  })
+
+  it('Cypress logo has a valid href ', () => {
+    cy.get('header * a[href="/"]')
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.request(href).its('status').should('eq', 200)
+      })
+  })
+})
